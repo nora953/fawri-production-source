5f5ed5294667dd5dc39654c3d98df768c7832a64
nora953 <45720986+nora953@users.noreply.github.com>
2026-08-08 20:55:56 +0300

    Keep subscription guarantee off frozen schema chain

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -3,7 +3,6 @@ export * from "./accounts";
 export * from "./merchants";
 export * from "./sessions";
 export * from "./subscriptions";
-export * from "./subscription-guarantee";
 export * from "./catalog";
 export * from "./channels";
 export * from "./conversations";
