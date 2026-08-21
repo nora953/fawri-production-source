22ad43201b66b1fffd567460828501cd8c79c238
nora953 <45720986+nora953@users.noreply.github.com>
2026-08-22 02:43:37 +0300

    Export provider cost schema

--- lib/db/src/schema/index.ts
+++ lib/db/src/schema/index.ts
@@ -5,6 +5,7 @@ export * from "./merchant-management";
 export * from "./sessions";
 export * from "./subscriptions";
 export * from "./saas-billing";
+export * from "./provider-costs";
 export * from "./catalog";
 export * from "./channels";
 export * from "./conversations";
