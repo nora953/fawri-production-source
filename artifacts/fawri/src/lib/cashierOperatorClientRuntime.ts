66d22a5e0d0ba890cd0f89912e1ed2545e378165
nora953 <45720986+nora953@users.noreply.github.com>
2026-08-26 04:52:53 +0300

    Point cashier client barrel to hardened runtimes

--- artifacts/fawri/src/lib/cashierOperatorClientRuntime.ts
+++ artifacts/fawri/src/lib/cashierOperatorClientRuntime.ts
@@ -1,3 +1,3 @@
 export * from './cashierOperatorLocalSecurity';
-export * from './cashierOperatorSessionClient';
+export * from './cashierOperatorSessionRuntime';
 export * from './cashierOperatorCloudSync';
