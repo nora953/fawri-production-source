80743c1235e64719a0381c2d53f1107b21db9a7e
nora953 <45720986+nora953@users.noreply.github.com>
2026-09-02 23:58:02 +0300

    Apply project-wide centered form field content

--- artifacts/fawri/src/main.tsx
+++ artifacts/fawri/src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import "./styles/fawriUiBaseline.css";
 import "./styles/catalogSummaryCards.css";
 import "./styles/fawriLanguageAuthority.css";
+import "./styles/fawriFieldContent.css";
 import { I18nProvider } from "@/lib/i18n";
 
 createRoot(document.getElementById("root")!).render(
