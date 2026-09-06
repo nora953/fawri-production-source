9cecad4370f10f05c855fce5c9b92f4f2cca25d6
nora953 <45720986+nora953@users.noreply.github.com>
2026-09-06 22:37:31 +0300

    Apply final Arabic catalog wording polish

--- artifacts/fawri/src/main.tsx
+++ artifacts/fawri/src/main.tsx
@@ -9,10 +9,12 @@ import { installAdaptiveIdentifierInputs } from "@/lib/adaptiveIdentifierInputs"
 import { installCatalogEditorAutoDirection } from "@/lib/catalogEditorAutoDirection";
 import { installCatalogArabicServiceGoldenGuard } from "@/lib/catalogArabicServiceGoldenGuard";
 import { installCatalogDetailsTypeParity } from "@/lib/catalogDetailsTypeParity";
+import { applyCatalogEditorArabicFinalSemanticParity } from "@/lib/catalogEditorArabicFinalSemanticParity";
 import { applyCatalogEditorEnglishSemanticParity } from "@/lib/catalogEditorEnglishSemanticParity";
 import { applyCatalogEditorSoraniSemanticParity } from "@/lib/catalogEditorSoraniSemanticParity";
 import { I18nProvider } from "@/lib/i18n";
 
+applyCatalogEditorArabicFinalSemanticParity();
 applyCatalogEditorEnglishSemanticParity();
 applyCatalogEditorSoraniSemanticParity();
 installAdaptiveIdentifierInputs();
